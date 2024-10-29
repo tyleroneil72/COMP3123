9de82e94f26f8f3ffcbfa08d2ddf44cde28b6c20
Tyler O'Neil <tyleroneildev@gmail.com>
2024-10-29 18:43:40 -0400

    Finish Lab Seven

---
+++ labSeven/labweek09/src/App.js
@@ -0,0 +1,19 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <h1>Welcome to fullstack development - I</h1>
+        <h2>React Js Programming Week09 Lab Exercise</h2>
+        <h3>101434244</h3>
+        <h4>Tyler O'Neil</h4>
+        <h5>George Brown College, Toronto</h5>
+      </header>
+    </div>
+  );
+}
+
+export default App;
